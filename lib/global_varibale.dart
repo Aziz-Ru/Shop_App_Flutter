f52906ba34062ef023c6de695c2c673fc8e605c8
Aziz <siliconoxide4@gmail.com>
2024-07-17 21:59:02 +0600

    updated

---
+++ lib/global_varibale.dart
@@ -0,0 +1,44 @@
+final products = [
+  {
+    'id': 1,
+    'title': 'Men\'s Nike Shoes',
+    'company': 'Nike',
+    'price': 24.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/malva.jpg'
+  },
+  {
+    'id': 2,
+    'title': 'Men\'s Adidas Shoes',
+    'company': 'Adidas',
+    'price': 42.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/malva.jpg'
+  },
+  {
+    'id': 3,
+    'title': 'Women\'s Nike Shoes',
+    'company': 'Nike',
+    'price': 32.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/malva.jpg'
+  },
+  {
+    'id': 4,
+    'title': 'Men\'s Puma Shoes',
+    'company': 'Puma',
+    'price': 52.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/malva.jpg'
+  },
+];
+
+final cart = [
+  {
+    'id': 1,
+    'title': 'Puma Shoes',
+    'company': 'Puma',
+    'price': 352.52,
+    'sizes': 8,
+  }
+];
---
+++ lib/product_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final double price;
+  final String image;
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.price, required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [Text(title),
+         const SizedBox(height: 5),
+          Text('\$$price'),
+          Image(image: AssetImage(image),),
+        ],
+      ),
+    );
+  }
+}
